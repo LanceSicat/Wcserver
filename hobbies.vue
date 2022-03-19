5fa2b84c8266242cae57d18256d35ae8ea9009d5
LanceSicat <99706371+LanceSicat@users.noreply.github.com>
2022-03-20 00:02:05 +0800

    Add files via upload

---
+++ hobbies.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="hobbies">
+        <div id="right-info">
+            <div class="container">
+                <div class="hobbies" role="alert">
+                    <ul>
+                        <li>{{ l1 }}</li>
+                        <li>{{ l2 }}</li>
+                        <li>{{ l3 }}</li>
+                        <li>{{ l4 }}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'hobbies',
+    data () {
+        return {
+            l1: 'Biking',
+            l2: 'Watching Movie',
+            l3: 'Playing Basketball',
+            l4: 'Playing Computer Games'
+        }
+    }
+}
+</script>
